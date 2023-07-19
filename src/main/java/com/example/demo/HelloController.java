6da8ef5b62b1c2a9dd97b93a8be6b9c31119fe80
guguttemy <guguttemy@gmail.com>
2023-07-19 18:26:01 +0900

    Add controller

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package dev.sample.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Hello Jenkins!";
+    }
+}
